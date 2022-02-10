9bd796b9f54c60d7423728239d0f9e797b7ec3ab
hakunagi <hakunagi.rei@gmail.com>
2022-02-10 23:40:17 +0900

    なんとjsがスリムに

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import "./style.scss"
 {
-  const g = (q) => document.getElementById(q)
-  const a = (q, w, e) => document.getElementById(q).addEventListener(w, e)
-  const func = [
-    [
-      (v) => decodeURIComponent(escape(atob(v))),
-      (v) => btoa(unescape(encodeURIComponent(v))),
-    ],
-    [(v) => decodeURI(v), (v) => encodeURI(v)],
+  const g = (v) => document.getElementById(v)
+  const d = decodeURIComponent
+  const e = encodeURIComponent
+  const v = "value"
+  const f = [
+    [(v) => d(escape(atob(v))), (v) => btoa(unescape(e(v)))],
+    [d, e],
   ]
-  let sel = 0
-  a("s", "change", function () {
-    sel = this.value
-  })
-  a("D", "click", () => {
-    g("e").value = func[sel][0](g("d").value)
-  })
-  a("E", "click", () => {
-    g("d").value = func[sel][1](g("e").value)
-  })
+  let s = 0
+  g("s").onchange = function () {
+    s = this[v]
+  }
+  g("D").onclick = () => {
+    g("e")[v] = f[s][0](g("d")[v])
+  }
+  g("E").onclick = () => {
+    g("d")[v] = f[s][1](g("e")[v])
+  }
 }
