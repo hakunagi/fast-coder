dcf8fff68e7180f980efb6d454e1c4fd79d5e62d
hakunagi <hakunagi.rei@gmail.com>
2022-02-10 23:43:44 +0900

    なんと更にjsがスリムに

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import "./style.scss"
-{
-  const g = (v) => document.getElementById(v)
-  const d = decodeURIComponent
-  const e = encodeURIComponent
-  const v = "value"
-  const f = [
-    [(v) => d(escape(atob(v))), (v) => btoa(unescape(e(v)))],
-    [d, e],
-  ]
-  let s = 0
-  g("s").onchange = function () {
-    s = this[v]
-  }
-  g("D").onclick = () => {
-    g("e")[v] = f[s][0](g("d")[v])
-  }
-  g("E").onclick = () => {
-    g("d")[v] = f[s][1](g("e")[v])
-  }
+let g = (v) => document.getElementById(v)
+let d = decodeURIComponent
+let e = encodeURIComponent
+let v = "value"
+let f = [
+  [(v) => d(escape(atob(v))), (v) => btoa(unescape(e(v)))],
+  [d, e],
+]
+let s = 0
+g("s").onchange = function () {
+  s = this[v]
+}
+g("D").onclick = () => {
+  g("e")[v] = f[s][0](g("d")[v])
+}
+g("E").onclick = () => {
+  g("d")[v] = f[s][1](g("e")[v])
 }
